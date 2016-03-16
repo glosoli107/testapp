e7bd54038260a00ba293d112956d8a20335020c2
glosoli107 <ahiltner@bu.edu>
2016-03-15 21:30:59 -0400

    bundler edit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-server '104.236.75.183', user: 'deploy', roles: %w{web app}
+server '104.236.75.183', user: 'deploy', roles: %w{web app db}
 
 # server-based syntax
 # ======================
