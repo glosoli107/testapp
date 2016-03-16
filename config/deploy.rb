33d87fd017ab0c2989001322030cdba86370a40c
glosoli107 <ahiltner@bu.edu>
2016-03-15 21:06:56 -0400

    bundler edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
+require 'bundler/capistrano'
+
 set :application, 'TestApp'
 set :repo_url, 'git@github.com:glosoli107/testapp.git'
 
